1898edb947fda2a37be9beb7f6a76ffc1d78d3e2
Scott James <floridaconsulting@gmail.com>
2017-11-03 21:27:49 -0400

    add template for credentials header for RFID reader Arduino sketch

---
+++ RFIDHttpClient/credentials-template.h
@@ -0,0 +1,12 @@
+
+
+// copy this to private_credentials.h
+// set ssid/wpakey to those used on the Raspi 3
+// the Pi creates its own access point for the reader
+// this is NOT your local LAN access point
+
+// also set your .gitconfig to not check in private_* files
+
+char* ssid = "pi_ap_ssid";
+char* wpakey = "pi_ap_passwd";
+
